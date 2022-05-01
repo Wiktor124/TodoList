0064997b8790c67b78e5ce0fa923a2dc7b397d73
Víctor peña <victortkd88@gmail.com>
2022-05-01 11:13:14 -0600

    new properties

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 const render = () => {
   const todoList = document.getElementById("todo-list");
-  const todoTemplate = todos.map((t) => `<li class="chores">${t}   press here to delete</li>`);
+  const todoTemplate = todos.map((t) => `<li class="chores">${t}   delete</li>`);
   todoList.innerHTML = todoTemplate.join(" ");
   const elements = document.querySelectorAll("#todo-list li");
   elements.forEach((element, i) => {
