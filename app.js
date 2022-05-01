22fc674160bf571354e75bde0da574cced3b3684
Víctor peña <victortkd88@gmail.com>
2022-05-01 10:55:48 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+const render = () => {
+  const todoList = document.getElementById("todo-list");
+  const todoTemplate = todos.map((t) => `<li class="chores">${t}</li>`);
+  todoList.innerHTML = todoTemplate.join(" ");
+  const elements = document.querySelectorAll("#todo-list li");
+  elements.forEach((element, i) => {
+    element.addEventListener("click", () => {
+      element.parentNode.removeChild(element);
+      todos.splice(i, 1);
+      render(updateTodos(todos));
+    });
+  });
+};
+
+const updateTodos = (todos) => {
+  const todoString = JSON.stringify(todos);
+  localStorage.setItem("todos", todoString);
+};
+
+window.onload = () => {
+  render();
+  const form = document.getElementById("todo-form");
+  form.onsubmit = (e) => {
+    e.preventDefault();
+    const todo = document.getElementById("todo");
+    const todoText = todo.value;
+    todo.value = "";
+    todos.push(todoText);
+    render(updateTodos(todos));
+  };
+};
